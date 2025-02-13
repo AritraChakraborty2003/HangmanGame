ef387d19a6e7384fe6daf623f6ba8f2ec198c254
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-02-13 18:15:14 +0530

    Feat: Added a modal feature

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HangmanImage from "./HangmanImage";
 import TextArea from "./TextArea";
 import Hint from "./Hint";
@@ -8,18 +8,93 @@ import Keyboard from "./Keyboard";
 import { useContext } from "react";
 import { GameContext } from "../GameContext/GameContext";
 import { countUnique } from "../Utils/countUnique";
-import Result from "./Result";
+
+import Modal from "react-modal";
+Modal.setAppElement("#root"); // Recommended for accessibility
 
 const Main = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const closeModal = (): void => setIsOpen(false);
+
+  interface ModalStyles {
+    overlay: {
+      backgroundColor: string;
+    };
+    content: {
+      background: string;
+      borderRadius: string;
+      padding: string;
+      maxWidth: string;
+      maxHeight: string;
+      margin: string;
+    };
+  }
+
+  let modalStyles: ModalStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.75)", // Dark overlay
+    },
+    content: {
+      background: "white",
+      borderRadius: "10px",
+      padding: "10px",
+      maxWidth: "400px",
+      maxHeight: "350px",
+      margin: "auto",
+    },
+  };
+  if (screen.width >= 1920) {
+    modalStyles = {
+      overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.75)", // Dark overlay
+      },
+      content: {
+        background: "white",
+        borderRadius: "10px",
+        padding: "10px",
+        maxWidth: "600px",
+        maxHeight: "450px",
+        margin: "auto",
+      },
+    };
+  } else if (screen.width > 1000 && screen.width < 1920) {
+    modalStyles = {
+      overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.75)", // Dark overlay
+      },
+      content: {
+        background: "white",
+        borderRadius: "10px",
+        padding: "10px",
+        maxWidth: "400px",
+        maxHeight: "320px",
+        margin: "auto",
+      },
+    };
+  } else {
+    modalStyles = {
+      overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.75)", // Dark overlay
+      },
+      content: {
+        background: "white",
+        borderRadius: "10px",
+        padding: "10px",
+        maxWidth: "400px",
+        maxHeight: "260px",
+        margin: "auto",
+      },
+    };
+  }
   const {
     selectedWord,
     correctChosen,
     isWin,
     setIsWin,
     incorrect,
-    isLose,
     setIsLose,
-    isGameOver,
+
     setIsGameOver,
   } = useContext(GameContext);
 
@@ -28,11 +103,11 @@ const Main = () => {
     if (correctChosen.length === len) {
       setIsWin(true);
       setIsGameOver(true);
-      console.log("Win");
+      setIsOpen(true);
     } else if (incorrect === 6) {
       setIsLose(true);
       setIsGameOver(true);
-      console.log("Lose");
+      setIsOpen(true);
     }
   };
 
@@ -41,30 +116,71 @@ const Main = () => {
   }, [correctChosen, incorrect, selectedWord]);
   return (
     <>
-      {(!isGameOver && (
-        <div className="MainArea bg-[#FFB22C] w-[100vw] h-[100vh] flex justify-center items-center absolute">
-          <div className="playArea w-[92%] absolute flex h-[98%] lg:h-[94%] bg-white rounded-xl">
-            <div className="absolute left  w-[100%] lg:w-[50%]  h-[45%] lg:h-[100%]">
-              <HangmanImage />
-            </div>
-            {(screen.width <= 350 && (
-              <div className="right relative top-[47%] lg:left-[50%] lg:top-[0%]  w-[100%] lg:w-[50%]  h-[53%] lg:h-[100%] border-t-[0.5px]">
-                <TextArea />
-                <Hint />
-                <ChanceArea />
-                <Keyboard />
-              </div>
-            )) || (
-              <div className="right relative top-[47%] lg:left-[50%] lg:top-[0%]  w-[100%] lg:w-[50%]  h-[53%] lg:h-[100%] border-t-[0.5px] border-l-[0.5px] overflow-y-hidden">
-                <TextArea />
-                <Hint />
-                <ChanceArea />
-                <Keyboard />
-              </div>
-            )}
+      <div className="MainArea bg-[#FFB22C] w-[100vw] h-[100vh] flex justify-center items-center absolute">
+        <div className="playArea w-[92%] absolute flex h-[98%] lg:h-[94%] bg-white rounded-xl">
+          <div className="absolute left  w-[100%] lg:w-[50%]  h-[45%] lg:h-[100%]">
+            <HangmanImage />
           </div>
+          {(screen.width <= 350 && (
+            <div className="right relative top-[47%] lg:left-[50%] lg:top-[0%]  w-[100%] lg:w-[50%]  h-[53%] lg:h-[100%] border-t-[0.5px]">
+              <TextArea />
+              <Hint />
+              <ChanceArea />
+              <Keyboard />
+            </div>
+          )) || (
+            <div className="right relative top-[47%] lg:left-[50%] lg:top-[0%]  w-[100%] lg:w-[50%]  h-[53%] lg:h-[100%] border-t-[0.5px] border-l-[0.5px] overflow-y-hidden">
+              <TextArea />
+              <Hint />
+              <ChanceArea />
+              <Keyboard />
+            </div>
+          )}
         </div>
-      )) || <Result isWin={isWin} isLose={isLose} />}
+      </div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        style={modalStyles}
+        shouldCloseOnOverlayClick={false}
+      >
+        <div
+          className="flex flex-col justify-center items-center"
+          style={{ background: "white" }}
+        >
+          <img
+            src={isWin ? "happy.png" : "lose.png"}
+            height={isWin ? 200 : 120}
+            width={isWin ? 200 : 120}
+            style={{ textAlign: "center", marginTop: "4vmin" }}
+          />
+          <p
+            className=""
+            style={{
+              marginTop: "4vmin",
+              color: "rgba(0, 0, 0, 0.75)",
+              fontSize: "3vmin",
+              fontFamily: "poppins",
+            }}
+          >
+            {isWin ? "Hurray!!! You Won " : "Oops! You Lose "}
+          </p>
+          <button
+            onClick={() => location.reload()}
+            className="2xl:text-[2.45vmin]"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.75)",
+              color: "white",
+              padding: "1.15vmin",
+              marginTop: "4vmin",
+            }}
+          >
+            Replay
+          </button>
+        </div>
+      </Modal>
+      ;
     </>
   );
 };
